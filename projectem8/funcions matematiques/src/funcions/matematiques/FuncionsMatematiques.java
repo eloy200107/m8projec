f75a217b4d60d5778e8a422296da6ef2b216cdf9
eloy200107 <119842178+eloy200107@users.noreply.github.com>
2023-05-12 12:46:02 +0200

    Add files via upload

---
+++ projectem8/funcions matematiques/src/funcions/matematiques/FuncionsMatematiques.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funcions.matematiques;
+
+/**
+ *
+ * @author alumne
+ */
+public class FuncionsMatematiques {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static double calcularMedia(double[] numeros) {
+        double suma = 0;
+        for (double numero : numeros) {
+            suma += numero;
+        }
+        return suma / numeros.length;
+    }
+    public class Main {
+    public static void main(String[] args) {
+        double[] numeros = {1.5, 2.7, 3.1, 4.8, 5.2};
+        double media = FuncionsMatematiques.calcularMedia(numeros);
+        System.out.println("La media de los números es: " + media);
+    }
+}
+
+}
+    
+    
+
